836b185b58d07733c2322bda22439b61fe54014b
Owen Campbell <owcampbe@microsoft.com>
2025-07-21 14:29:05 -0700

    Add start of number parsing

--- src/client/lib/lang/parser.ts
+++ src/client/lib/lang/parser.ts
@@ -1,5 +1,13 @@
 import { PeekableIterator, error, isErr, isOk, ok, type Result } from "./core";
-import type { Expr, Program } from "./types";
+import type { Expr, List, Program } from "./types";
+
+function skipWhitespace(it: PeekableIterator<string>) {
+  it.skipWhile((c) => c.trim() === "");
+}
+
+function isNumber(str: string): boolean {
+  return !Number.isNaN(Number(str)) && str.trim() !== "";
+}
 
 export function parse(input: string): Result<Program, Error[]> {
   const iterator = new PeekableIterator(Array.from(input));
@@ -35,20 +43,20 @@ function parse_inner(it: PeekableIterator<string>): Result<Expr, Error[]> {
     return error(errors);
   }
 
-  it.skipWhile((c) => c.trim() === "");
+  skipWhitespace(it);
 
-  if (!it.hasNext()) {
+  const next = it.peek();
+  if (next === undefined) {
     errors.push(new Error(`Unexpected end of input at position ${location}`));
     return error(errors);
   }
 
-  const next = it.peek();
-  console.log(`Peeking: ${next}`);
-
   if (next === "(") {
     return parse_list(it);
   }
-
+  if (isNumber(next) || (next === "-" && isNumber(it.peek(1) ?? ""))) {
+    return parse_number(it);
+  }
   errors.push(
     new Error(`Unexpected character ${next} at position ${location}`),
   );
@@ -57,11 +65,10 @@ function parse_inner(it: PeekableIterator<string>): Result<Expr, Error[]> {
   return error(errors);
 }
 
-function parse_list(it: PeekableIterator<string>): Result<Expr, Error[]> {
+function parse_list(it: PeekableIterator<string>): Result<List, Error[]> {
   const exprs: Expr[] = [];
   const errors: Error[] = [];
-
-  console.log(`Parsing list`);
+  const location = it.pos();
 
   const next = it.next();
   if (next !== "(") {
@@ -78,9 +85,10 @@ function parse_list(it: PeekableIterator<string>): Result<Expr, Error[]> {
 
     exprs.push(res.value);
 
-    it.skipWhile((c) => c.trim() === "");
+    skipWhitespace(it);
     const next = it.peek();
     if (next === ",") {
+      it.next();
       continue;
     }
     if (next === ")") {
@@ -99,5 +107,9 @@ function parse_list(it: PeekableIterator<string>): Result<Expr, Error[]> {
     return error(errors);
   }
 
-  return ok({ type: "list", location: 0, elements: exprs } as Expr);
+  const list: List = { type: "list", location, elements: exprs };
+
+  return ok(list);
 }
+
+function parse_number();
