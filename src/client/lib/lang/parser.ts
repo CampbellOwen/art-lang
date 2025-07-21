117f4f5d61183c3d0968e7b261a5627319f5f4f2
Owen Campbell <owcampbe@microsoft.com>
2025-07-21 12:00:20 -0700

    Move Result code

--- src/client/lib/lang/parser.ts
+++ src/client/lib/lang/parser.ts
@@ -1,6 +1,6 @@
-import type { Result } from "./core";
+import { error, type Result } from "./core";
 import type { Program } from "./types";
 
 export function parse(_input: string): Result<Program, Error[]> {
-  return { type: "error", value: [new Error("Unimplemented")] };
+  return error([new Error("unimplemented")]);
 }
--- src/client/lib/lang/types.ts
+++ src/client/lib/lang/types.ts
@@ -5,7 +5,3 @@ export type Expr =
   | { type: "list"; elements: Expr[] };
 
 export type Program = Expr[];
-
-export type Ok<T> = { type: "ok"; value: T };
-export type Err<E> = { type: "error"; value: E };
-export type Result<T, E> = Ok<T> | Err<E>;
