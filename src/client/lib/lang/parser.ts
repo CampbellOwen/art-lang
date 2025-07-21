47cad0174fe56be5ef4aec1083fc61d88d36f434
Owen Campbell <owcampbe@microsoft.com>
2025-07-21 12:11:45 -0700

    Start of parse looop

--- src/client/lib/lang/parser.ts
+++ src/client/lib/lang/parser.ts
@@ -1,6 +1,22 @@
-import { error, type Result } from "./core";
-import type { Program } from "./types";
+import { error, ok, type Result } from "./core";
+import type { Expr, Program } from "./types";
 
-export function parse(_input: string): Result<Program, Error[]> {
-  return error([new Error("unimplemented")]);
+export function parse(input: string): Result<Program, Error[]> {
+  const iterator = Array.from(input).entries();
+
+  const errors: Error[] = [];
+  const program: Expr[] = [];
+
+  let next;
+  do {
+    next = iterator.next();
+    if (!next.value) {
+    }
+  } while (!next.done);
+
+  if (errors.length > 0) {
+    return error(errors);
+  }
+
+  return ok(program);
 }
