4b00cd0a79831d7862d5c9ad7d13e246b6a39a18
Owen Campbell <owcampbe@microsoft.com>
2025-07-21 14:28:59 -0700

    factor out types

--- src/client/lib/lang/types.ts
+++ src/client/lib/lang/types.ts
@@ -1,7 +1,8 @@
-export type Expr =
-  | { type: "number"; location: number; value: number }
-  | { type: "string"; location: number; value: string }
-  | { type: "symbol"; location: number; value: string }
-  | { type: "list"; location: number; elements: Expr[] };
+export type Num = { type: "number"; location: number; value: number };
+export type Str = { type: "string"; location: number; value: string };
+export type Symbol = { type: "symbol"; location: number; value: string };
+export type List = { type: "list"; location: number; elements: Expr[] };
+
+export type Expr = Num | Str | Symbol | List;
 
 export type Program = Expr[];
